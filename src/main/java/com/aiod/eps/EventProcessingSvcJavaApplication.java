8a6e9c506672528f153e85caa2d632028025453a
Sunil Yadav <aiodsunil@gmail.com>
2019-10-19 19:25:26 +0100

    inital commit

---
+++ src/main/java/com/aiod/eps/EventProcessingSvcJavaApplication.java
@@ -0,0 +1,13 @@
+package com.aiod.eps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventProcessingSvcJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventProcessingSvcJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aiod/eps/EventProcessingSvcJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aiod.eps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventProcessingSvcJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
